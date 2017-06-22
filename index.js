5e0adf769852165835d5600af00212769ddd407a
axetroy <troy450409405@gmail.com>
2017-06-22 14:26:49 +0800

    feat: promisify the wx Object

--- index.js
+++ index.js
@@ -8,9 +8,42 @@ import Component from './component';
 import PropTypes from './prop-types';
 import _createPage from './create-page';
 
+function promisify(wxx) {
+  wxx = {...wxx};
+  for (let attr in wxx) {
+    if (wxx.hasOwnProperty(attr) && typeof wxx[attr] === 'function') {
+      if (/sync$/.test(attr)) {
+        // skip over the sync method
+      }
+      if (wxx[attr + 'Async']) {
+        console.warn(`${attr + 'Async'} have been defined in ${wxx}`)
+      } else {
+        wxx[attr + 'Async'] = function asyncFunction(argv = {}) {
+          return new Promise(function (resolve, reject) {
+            wxx[attr].call(wxx, {
+              ...argv,
+              ...{
+                success(res){
+                  resolve(res);
+                },
+                fail(err){
+                  debugger;
+                  reject(err);
+                }
+              }
+            })
+          })
+        };
+      }
+    }
+  }
+  return wxx;
+}
+
 const labrador = {
   // 原始wx对象
   wx,
+  ...promisify(wx),
   // getApp() 优雅的封装
   get app() {
     return getApp();
