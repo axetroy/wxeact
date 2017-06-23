7c733c7437f9216792f33646404d5a7011c455f0
axetroy <troy450409405@gmail.com>
2017-06-23 22:00:05 +0800

    feat(http): define http property in wxeact object once you ref the wxapp-http module

--- packages/wxeact-http/index.js
+++ packages/wxeact-http/index.js
@@ -1,3 +1,11 @@
+import wx from 'wxeact';
 import http from 'wxapp-http/index';
+
+Object.defineProperty(wx, 'http', {
+  configurable: false,
+  enumerable: false,
+  writable: false,
+  value: http
+});
 
 export default http;
