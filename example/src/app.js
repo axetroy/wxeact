b8e0aecf018b10d3f976572b3151347803ab1c29
axetroy <troy450409405@gmail.com>
2017-06-23 14:56:53 +0800

    refactor: example替换为wxeact-http模块

--- example/src/app.js
+++ example/src/app.js
@@ -1,4 +1,6 @@
 import { setStore } from 'wxeact-redux';
+import http from 'wxeact-http';
+
 import store from './redux';
 
 if (__DEV__) {
@@ -10,6 +12,21 @@ setStore(store);
 
 export default class {
   async onLaunch() {
+    function successHandler(res) {
+      console.log(res);
+    }
+    function errorHandler(err) {
+      console.error(err);
+    }
+
+    // 测试http模块，发送50个请求
+    for (let i = 0; i < 50; i++) {
+      http
+        .get('http://www.baidu.com/?v=' + (i + 1))
+        .then(successHandler)
+        .catch(errorHandler);
+    }
+
     console.info('App 启动...');
   }
 }
--- example/src/redux/createStore.js
+++ example/src/redux/createStore.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { persistStore, autoRehydrate } from 'redux-persist';
-import createLogger from 'redux-logger';
+import { createLogger } from 'redux-logger';
 import reduxPersist from '../config/redux-persist';
 import * as StartupActions from './startup';
 
@@ -10,15 +10,20 @@ import * as StartupActions from './startup';
  * @param rootSaga
  * @returns {*}
  */
-export default function (rootReducer, rootSaga) {
+export default function(rootReducer, rootSaga) {
   const middleware = [];
   const enhancers = [];
 
   // log中间件
-  const SAGA_LOGGING_BLACKLIST = ['EFFECT_TRIGGERED', 'EFFECT_RESOLVED', 'EFFECT_REJECTED'];
+  const SAGA_LOGGING_BLACKLIST = [
+    'EFFECT_TRIGGERED',
+    'EFFECT_RESOLVED',
+    'EFFECT_REJECTED'
+  ];
   if (__DEV__) {
     const logger = createLogger({
-      predicate: (getState, { type }) => SAGA_LOGGING_BLACKLIST.indexOf(type) === -1
+      predicate: (getState, { type }) =>
+        SAGA_LOGGING_BLACKLIST.indexOf(type) === -1
     });
     middleware.push(logger);
   }
@@ -32,7 +37,9 @@ export default function (rootReducer, rootSaga) {
   const store = createStore(rootReducer, compose(...enhancers));
 
   // persist
-  persistStore(store, reduxPersist, () => store.dispatch(StartupActions.startup()));
+  persistStore(store, reduxPersist, () =>
+    store.dispatch(StartupActions.startup())
+  );
 
   return store;
 }
